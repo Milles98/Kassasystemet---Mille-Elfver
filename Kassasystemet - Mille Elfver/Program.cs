a954c1a225389b5259723ab61b081dac973096d5
Mille Elfver <mille-elfver98@hotmailc.om>
2023-09-11 08:23:33 +0200

    Add project files.

---
+++ Kassasystemet - Mille Elfver/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Kassasystemet___Mille_Elfver
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Programmera ett kassasystem - (som man har i kassan i en matbutik)
+        }
+    }
+}
