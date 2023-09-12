c3aabc07e66c0a8fb72bd788bcd4d8f842fb1542
Mille Elfver <mille-elfver98@hotmailc.om>
2023-09-12 13:39:21 +0200

    Lagt in fler kommentarer

--- Kassasystemet - Mille Elfver/Program.cs
+++ Kassasystemet - Mille Elfver/Program.cs
@@ -5,6 +5,9 @@
         static void Main(string[] args)
         {
             //Programmera ett kassasystem - (som man har i kassan i en matbutik)
+            //använd double till talen (avrunar typ banan till 19.50 (2 decimaler) kan använda Math.Round med 2 decimaler
+            //man får dock använda int istället om det blir jobbigt
+            //Kan göra kvitto tabell med \t i textraderna (betyder tab så de hoppar)
         }
     }
 }
