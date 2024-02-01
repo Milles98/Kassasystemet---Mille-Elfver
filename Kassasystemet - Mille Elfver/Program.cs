4354bd4e18fefa4be223d5b4eb6c73def0dd34c3
Mille Elfver <mille.elfver98@gmail.com>
2024-02-01 06:45:10 +0100

    Commented away the starting statement

--- Kassasystemet - Mille Elfver/Program.cs
+++ Kassasystemet - Mille Elfver/Program.cs
@@ -10,12 +10,12 @@ namespace Kassasystemet___Mille_Elfver
         {
             Console.OutputEncoding = System.Text.Encoding.UTF8;
 
-            Console.WriteLine("Förord");
-            Console.WriteLine("1. Öppna konsolen i helskärm för bättre användarupplevelse.");
-            Console.WriteLine("2. Jag har haft problem med windows konsolen/terminalen.");
-            Console.WriteLine("Om det ser buggigt ut ändra terminalinställning från windows terminal till windows konsolvärd.");
-            Console.WriteLine("Tryck valfri knapp för att fortsätta.");
-            Console.ReadKey();
+            //Console.WriteLine("Förord");
+            //Console.WriteLine("1. Öppna konsolen i helskärm för bättre användarupplevelse.");
+            //Console.WriteLine("2. Jag har haft problem med windows konsolen/terminalen.");
+            //Console.WriteLine("Om det ser buggigt ut ändra terminalinställning från windows terminal till windows konsolvärd.");
+            //Console.WriteLine("Tryck valfri knapp för att fortsätta.");
+            //Console.ReadKey();
 
             using (var container = AutoFacRegistration.RegisteredContainers())
             {
